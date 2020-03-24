1d9345543c29cddaabce12d3489ac7ca0b503a89
Sandro Scalco <info@sandrodesign.ch>
2020-03-24 13:22:47 +0100

    miitmi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+import {
+  Platform,
+  AlertController,
+  NavController,
+  ToastController,
+  ModalController,
+  LoadingController
+} from '@ionic/angular';
+
+import {
+  Router,
+  ActivatedRoute
+} from '@angular/router';
+//Capacitor
+import {
+  Plugins
+} from '@capacitor/core';
+const {
+  Share,
+  Browser
+} = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +29,131 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  shareSocialShareOptions: any;
+  showSocialShare = false;
+  chat: any;
+  constructor(
 
-  constructor() {}
+    public router: Router,
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
 
+    public toastController: ToastController,
+    public loadingController: LoadingController,
+
+    public modalController: ModalController,
+    public plt: Platform,
+    public alertController: AlertController,
+  ) {
+
+
+    this.chat = {
+      "url": "https://meet.jit.si/miitmi-",
+      "titel": "",
+      "name": "",
+      shared: false
+    };
+
+    this.route.queryParams.subscribe(params => {
+
+      console.log(params);
+
+      if (params.room ) {
+
+        this.chat = {
+          "url": "https://meet.jit.si/miitmi-",
+          "titel":  params.room,
+          //"name": params.name,
+          shared: false
+        };
+        this.openShare();
+     
+      }
+    });
+
+
+  }
+
+  async openChat() {
+    await Browser.open({ url: this.chat.url });
+  }
+
+
+  /*****************************************/
+  // SHARE
+  /*****************************************/
+
+  async openShare() {
+
+    let random = Math.random().toString(36).substring(2);
+    this.chat.url = this.chat.url + random;
+
+    console.log( this.chat);
+
+    if (this.plt.is('mobile') && navigator && navigator["share"]) {
+      await this.share(this.chat).catch(e=>{
+        this.chat.shared = false;
+      });
+     
+    } else {
+      await this.shareFallback(this.chat).catch(e=>{
+        this.chat.shared = false;
+      });
+    }
+    this.chat.shared = true;
+  };
+
+  async share(chat) {
+    let shareRet = await Share.share({
+      title: "miitmi " + chat.titel,
+      text:  "👋 treffe mich mit miitmii!",
+      url: chat.url,
+      dialogTitle: 'Miitmi! '
+    }).catch(onrejected => {
+      this.chat.shared = false;
+      
+
+    });
+  }
+
+  shareFallback(chat) {
+    return new Promise( (resolve) => {
+      let  share = chat.url;
+      if (chat.titel){
+        share = "👋 treffe mich mit miitmii!: " +  chat.url
+      }
+
+      console.log("text to share" + share);
+
+      // The configuration, set the share options
+      this.shareSocialShareOptions = {
+        displayNames: true,
+        config: [{
+          twitter: {
+            socialShareUrl:  share,
+            socialSharePopupWidth: 300,
+            socialSharePopupHeight: 400
+          }
+        }, {
+          email: {
+            socialShareBody: share
+          }
+        }, {
+          whatsapp: {
+            socialShareUrl: share
+          }
+        },{
+          facebook: {
+            socialShareUrl: share
+          }
+        },{
+          copy: {
+            socialShareUrl: share
+          }
+        }]
+      };
+      this.showSocialShare = true;
+      resolve();
+    });
+  }
 }
